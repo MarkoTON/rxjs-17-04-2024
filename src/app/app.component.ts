4f38a56cfc2c9c153bc06b2e8ba7991d04d5e752
markoton <markotoncanovic@gmail.com>
2024-04-17 13:20:43 +0200

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'rxjs-17-04-2024';
+}
